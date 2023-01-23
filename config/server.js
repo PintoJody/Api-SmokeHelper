5fafc53d21b7795e43b439fb4b02bcb873ecf8ae
Louis Cherik <louis.cherik@gmail.com>
2023-01-23 16:37:32 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7786809eb5c69b6a6626ac490c01d9fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '72b0355d-17e3-41f1-aa25-2c1844366a5c'
+};
